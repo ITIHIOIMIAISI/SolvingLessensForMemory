64c82ce9779f3ce7d0a909b1a584d42223d650a8
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-10 00:39:39 +0300

    Освежил значия по решению домашнего задания 4

---
+++ 002_ShowsMaxFrom3Number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Finding a max from 3 nubmers");
+Console.Write("Enter 1 number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter 2 number: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Enter 3 number: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (max < b)  max = b;
+    if (max < c) max = c;
+Console.Write($"Max nubmer : {max}");
