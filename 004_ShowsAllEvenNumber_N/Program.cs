06d1260a65187330bf0f468314715dd51b5da177
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-10 00:41:44 +0300

    Освежил значия по решению домашнего задания 8

---
+++ 004_ShowsAllEvenNumber_N/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("ShowsAllEvenNumber(N)");
+Console.Write("Enter nubmer : ");
+int nubmer = int.Parse(Console.ReadLine());
+
+for (int index = 0; index < nubmer; index++)
+{
+    if ((index % 2) == 0)
+        Console.Write($"{index} ");
+}
