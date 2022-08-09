5c99944fe93a61c937579a22f9c11fa7182b31a1
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-10 00:39:25 +0300

    Освежил значия по решению домашнего задания 2

---
+++ 001_ShowsMaxAndMinFromTwoNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Shows max and min from Two numbers");
+Console.Write("Enter one nubmer : ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter two nubmer : ");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = b;
+if (a < b) max = b; min = a;
+    Console.Write($"Max number : {max}, Min namber : {min}");
