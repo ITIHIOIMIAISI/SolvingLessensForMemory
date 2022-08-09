eeb3ad32f020875bfc7ea7b63ef545a8746a36d9
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-10 00:39:58 +0300

    Освежил значия по решению домашнего задания 6

---
+++ 003_IsItNumberEven/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Is it number even ?");
+Console.Write("Enter a number:  ");
+int nubmer = int.Parse(Console.ReadLine());
+if ((nubmer % 2) == 0) Console.WriteLine("Even");
+else Console.WriteLine("Odd");
